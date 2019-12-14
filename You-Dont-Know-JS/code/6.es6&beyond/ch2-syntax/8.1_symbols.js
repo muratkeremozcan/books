75d2e89d14e1da1183534e54267b5d4456bbbb2b
Ozcan <murat.ozcan@siemens.com>
2019-12-14 05:52:09 -0600

    symbols

---
+++ You-Dont-Know-JS/code/6.es6&beyond/ch2-syntax/8.1_symbols.js
@@ -0,0 +1,5 @@
+// unlike other primitive types, symbol does not have a literal form
+
+// simple primitives  : string  number  boolean  object  null   undefined ( SYMBOL got added  in ES6! )
+// complex primitives  : String  Number  Boolean  Object  Array  Date  RegExp  Error (all of type object)
+// side note, falsies : ''      0       false            null   undefined
