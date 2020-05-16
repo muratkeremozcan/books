2cc25a69afdebdad3f1363ccf3c9c171e1facf0e
Ozcan <murat.ozcan@siemens.com>
2020-05-16 06:48:54 -0500

    bounded context

--- Kyle-Simpson_Luis-Antecio/rxjs-in-action/examples/3/4/3_4.js
+++ Kyle-Simpson_Luis-Antecio/rxjs-in-action/examples/3/4/3_4.js
@@ -4,9 +4,9 @@
  *  @author Paul Daniels
  *  @author Luis Atencio
  */
-const isNumericalKeyCode = code => code >= 48 && code <= 57;
-const input = document.querySelector('#input');
-Rx.Observable.fromEvent(input, 'keyup')
-  .pluck('keyCode')
-  .filter(isNumericalKeyCode)
-  .subscribe(code => console.log(`User typed: ${String.fromCharCode(code)}`));
+const isNumericalKeyCode = code => code >= 48 && code <= 57; // ascii key code for 0-9
+const input = document.querySelector('#input'); // select id="input"
+Rx.Observable.fromEvent(input, 'keyup') // fromEvent is used to lift DOM events to the stream
+  .pluck('keyCode') // pluck is used to extract the value at property/key
+  .filter(isNumericalKeyCode) // filter/select for conditional logic (if numeric, output it)
+  .subscribe(code => console.log(`User typed: ${String.fromCharCode(code)}`)); // fromCharCode returns a string based on the character code: String.fromCharCode(65, 66, 67);  // returns "ABC"
