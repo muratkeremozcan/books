cea03a2ff2a82d0666fc2c4ce479ba1ae5f6c90d
Ozcan <murat.ozcan@siemens.com>
2020-07-21 06:25:08 -0500

    ch5 app

--- Angular_with_Typescript/fiddle/chapter5/di-samples/src/app/switching/mock.product.service.ts
+++
@@ -1,9 +0,0 @@
-import {Product} from "./product";
-import { Injectable } from "@angular/core";
-
-@Injectable()
-export class MockProductService {
-  getProduct(): Product {
-    return new Product('Samsung 7');
-  }
-}
