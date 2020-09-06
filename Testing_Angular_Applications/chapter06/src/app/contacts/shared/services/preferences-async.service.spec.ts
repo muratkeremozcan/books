4035e19c975ffca795b2e5f6ad8e4621247df9a8
Murat Ozcan <muratkerem@gmail.com>
2020-09-06 07:44:05 -0500

    testing services

--- Testing_Angular_Applications/chapter06/src/app/contacts/shared/services/preferences-async.service.spec.ts
+++ Testing_Angular_Applications/chapter06/src/app/contacts/shared/services/preferences-async.service.spec.ts
@@ -1,55 +1,56 @@
 import { TestBed, fakeAsync, flushMicrotasks, inject } from '@angular/core/testing';
 
-import { BrowserStorageAsync } from "./browser-storage.service";
+import { BrowserStorageAsync } from './browser-storage.service';
 import { PreferencesAsyncService } from './preferences-async.service';
 
+// [6] async service
+
 class BrowserStorageAsyncMock {
-    getItem = (property: string) => Promise.resolve({key: 'testProp', value: 'testValue'});
-    setItem = ({key: key, value: value}) => Promise.resolve(true);
+  getItem = (property: string) => Promise.resolve({key: 'testProp', value: 'testValue'});
+  setItem = ({key: key, value: value}) => Promise.resolve(true);
 }
 
-
 describe('PreferencesAsyncService', () => {
-    beforeEach(() => {
-        TestBed.configureTestingModule({
-            providers: [PreferencesAsyncService, {
-                provide: BrowserStorageAsync, useClass: BrowserStorageAsyncMock
-            }]
-        });
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [PreferencesAsyncService, {
+        provide: BrowserStorageAsync, useClass: BrowserStorageAsyncMock
+      }]
     });
+  });
 
-    it('should get a value', fakeAsync(inject([PreferencesAsyncService, BrowserStorageAsync],
-        (service: PreferencesAsyncService, browserStorage: BrowserStorageAsyncMock) => {
-            spyOn(browserStorage, 'getItem').and.callThrough();
+  it('should get a value', fakeAsync(inject([PreferencesAsyncService, BrowserStorageAsync],
+    (service: PreferencesAsyncService, browserStorage: BrowserStorageAsyncMock) => {
+      spyOn(browserStorage, 'getItem').and.callThrough();
 
-            let results, error;
+      let results, error;
 
-            service.getPropertyAsync('testProp')
-                .then(val => results = val)
-                .catch(err => error = err);
+      service.getPropertyAsync('testProp')
+        .then(val => results = val)
+        .catch(err => error = err);
 
-            flushMicrotasks();
+      flushMicrotasks();
 
-            expect(results.key).toEqual('testProp');
-            expect(results.value).toEqual('testValue');
-            expect(browserStorage.getItem).toHaveBeenCalledWith('testProp');
-            expect(error).toBeUndefined();
-        }))
-    );
+      expect(results.key).toEqual('testProp');
+      expect(results.value).toEqual('testValue');
+      expect(browserStorage.getItem).toHaveBeenCalledWith('testProp');
+      expect(error).toBeUndefined();
+    }))
+  );
 
 it('should throw an error if the key is missing', fakeAsync(inject([PreferencesAsyncService],
-    (service: PreferencesAsyncService) => {
+  (service: PreferencesAsyncService) => {
 
-        let result, error;
-        service.getPropertyAsync('')
-            .then(value => result = value)
-            .catch((err) => error = err);
+    let result, error;
+    service.getPropertyAsync('')
+      .then(value => result = value)
+      .catch((err) => error = err);
 
-        flushMicrotasks();
-        expect(result).toBeUndefined();
-        expect(error).toEqual('getPropertyAsync requires a property name');
+    flushMicrotasks();
+    expect(result).toBeUndefined();
+    expect(error).toEqual('getPropertyAsync requires a property name');
 
-    }))
+  }))
 );
 
 });
