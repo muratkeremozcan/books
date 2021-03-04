974561395f0a2a6d85c69d9474b15c17dace707e
Murat Ozcan <muratkerem@gmail.com>
2021-03-04 07:52:53 -0600

    added reactive form example

--- Angular_with_Typescript/angular-tour-of-heroes-complete/src/app/reactive-form-complex/reactive-form-complex.component.ts
+++ Angular_with_Typescript/angular-tour-of-heroes-complete/src/app/reactive-form-complex/reactive-form-complex.component.ts
@@ -9,6 +9,10 @@ import { Hero } from './hero';
 })
 export class ReactiveFormComplexComponent {
   heroForm: FormGroup;
+  powers = ['Really Smart', 'Super Flexible', 'Super Hot', 'Weather Changer'];
+  submitted = false;
+  model = new Hero('Dr IQ', this.powers[0], 'Chuck Overstreet');
+  myHero: Hero;
 
   constructor(fb: FormBuilder) {
     this.heroForm = fb.group({
@@ -19,42 +23,10 @@ export class ReactiveFormComplexComponent {
       ])
     });
   }
-  powers = ['Really Smart', 'Super Flexible', 'Super Hot', 'Weather Changer'];
-
-  // constructor(private fb: FormBuilder) {}
-  // heroForm = this.fb.group({
-  //   name: ['', Validators.required],
-  //   alterEgo: [''],
-  // });
-
-
-  model = new Hero(18, 'Dr IQ', this.powers[0], 'Chuck Overstreet');
-
-  submitted = false;
 
   onSubmit() {
-    alert('you submitted');
-    console.warn(this.heroForm.value)
+    console.warn(this.heroForm.value);
     this.submitted = true;
-  }
-
-  // TODO: Remove this when we're done
-  get diagnostic() {
-    return JSON.stringify(this.model);
-  }
-
-  newHero() {
-    this.model = new Hero(42, 'Dummy Hero', '');
-  }
-
-  skyDog(): Hero {
-    const myHero = new Hero(
-      42,
-      'SkyDog',
-      'Fetch any object at any distance',
-      'Leslie Rollover'
-    );
-    console.log('My hero is called ' + myHero.name); // "My hero is called SkyDog"
-    return myHero;
+    // TODO: get form values and display
   }
 }
