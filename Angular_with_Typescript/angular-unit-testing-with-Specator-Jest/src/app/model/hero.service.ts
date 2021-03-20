2122bd0570f764dc836e6a0537ebbd4d5f5e5f85
Murat Ozcan <muratkerem@gmail.com>
2021-03-20 08:07:10 -0500

    got some more working tests

--- Angular_with_Typescript/angular-unit-testing-with-Specator-Jest/src/app/model/hero.service.ts
+++ Angular_with_Typescript/angular-unit-testing-with-Specator-Jest/src/app/model/hero.service.ts
@@ -78,14 +78,12 @@ export class HeroService {
   private handleError<T>(operation = 'operation') {
     return (error: HttpErrorResponse): Observable<T> => {
 
-      // TODO: send the error to remote logging infrastructure
       console.error(error); // log to console instead
 
       const message = (error.error instanceof ErrorEvent) ?
         error.error.message :
        `server returned code ${error.status} with body "${error.error}"`;
 
-      // TODO: better job of transforming error for user consumption
       throw new Error(`${operation} failed: ${message}`);
     };
 
