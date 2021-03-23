dd14dbcc7dfc7595cde17b6b9dbaa9360fb596cf
Murat Ozcan <muratkerem@gmail.com>
2021-03-23 08:10:54 -0500

    working twain component spec with error checking

--- Angular_with_Typescript/angular-unit-testing-with-Specator-Jest/src/app/twain/twain.component.spec.ts
+++ Angular_with_Typescript/angular-unit-testing-with-Specator-Jest/src/app/twain/twain.component.spec.ts
@@ -1,29 +1,28 @@
-import { Spectator, createComponentFactory } from '@ngneat/spectator/jest';
+import { fakeAsync } from '@angular/core/testing';
+import { Spectator, createComponentFactory, byText } from '@ngneat/spectator/jest';
 
 import { of, throwError } from 'rxjs';
-import { last } from 'rxjs/operators';
 
 import { TwainComponent } from './twain.component';
 import { TwainService } from './twain.service';
 
 
-// [5] testing components that have external service dependencies - SPECTATOR/JEST MOCK (very similar to [2] testing service that have external service dependencies)
+// [5] testing components that have external service dependencies - SPECTATOR/JEST MOCK (very similar to [2] testing servicse that have external service dependencies)
 // setup the component much less overhead with spectator (5.1)
 // KEY contrast to [5] MANUAL MOCKs example: with spectator, you can fully mock the dependency using the mocks property (no need for custom mocks unless you need them) (5.1.2) . Similar to (2.4.0) testing services with other service dependencies.
 // KEY extra compared to [1] inject the service dependency:  depService = spectator.inject(DepService) (5.1.3) . Same as (2.4.1) inject the mock service dependency
 // stub the external service's return value, and exercise the main service under test (5.1.4) . Similar to (2.4.2) ,stubbing the external service and testing the service under test.
-// access the TS with spectator.component  (5.2)
+// access the TS with  (5.2)
 // use spectator.detectChanges()  to trigger the change detection (5.3),
-// use DOM testing library convenience methods:  https://github.com/ngneat/spectator#queries' (5.4)
+// if needed by the component implementation, use fakeAsync and spectator.tick() (5.4)
 // trigger the event using spectator events api https://github.com/ngneat/spectator#events-api (5.5.2) and verify what is emitted (5.5.3)
 
 
-describe('TwainComponent', () => {
+describe('[5] testing components that have external service dependencies - SPECTATOR/JEST MOCK', () => {
   let component: TwainComponent;
   let twainServiceSpy;
   let twainService;
-  let quoteEl;
-  let stubValue;
+  let testQuote;
 
   // (5.1) setup the component
   let spectator: Spectator<TwainComponent>;
@@ -47,24 +46,46 @@ describe('TwainComponent', () => {
 
     // (5.1.3) inject the service dependency:  depService = spectator.inject(DepService)
     twainServiceSpy = spectator.inject(TwainService);
-    stubValue = 'Test Quote';
-    // (5.1.4) stub the external service's return value, and exercise the component under test
-    twainService = jest.spyOn(twainServiceSpy, 'getQuote').mockReturnValue(of(stubValue));
-    // twainService = jest.spyOn(twainServiceSpy, 'getQuote').mockImplementation(() => of(stubValue)); // same
-    // twainService = twainServiceSpy.getQuote.mockImplementation(() =>() => of(stubValue)); // same
-
-    quoteEl = spectator.query('.twain').textContent;
   });
 
-  it('sanity', () => {
-    expect(component).toBeTruthy();
+  describe('(5.1.2) KEY contrast to manual mocks, use Spectator mocks, (5.1.3) inject the service dependency:  depService = spectator.inject(DepService)', () => {
+
+    beforeEach(() => {
+      // (5.1.4) stub the external service's return value, and exercise the component under test
+      testQuote = 'Test Quote';
+      twainService = jest.spyOn(twainServiceSpy, 'getQuote').mockReturnValue(of(testQuote));
+      // twainService = jest.spyOn(twainServiceSpy, 'getQuote').mockImplementation(() => of(testQuote)); // same
+      // twainService = twainServiceSpy.getQuote.mockImplementation(() =>() => of(testQuote)); // same
+    });
+
+    it('(5.1.4) stub the external service\'s return value, and exercise the component under test', () => {
+      expect(errorMessage()).toBeNull();
+      expect(twainService).not.toHaveBeenCalled();
+      expect(spectator.query(byText(testQuote))).toBeFalsy();
+    });
+
+    it('(5.3) use spectator.detectChanges()  to trigger the change detection ', () => {
+      spectator.detectChanges();
+      expect(twainService).toHaveBeenCalled();
+      expect(spectator.query('.twain')).toHaveText(testQuote);
+      expect(spectator.query(byText(testQuote))).toBeTruthy(); // same
+    });
   });
 
-  it('should not show quote before OnInit', () => {
-    expect(quoteEl).toBe('');
-    expect(errorMessage()).toBeNull();
-    expect(twainService).not.toHaveBeenCalled();
-  });
+  it('(5.4) if needed by the component implementation, use fakeAsync and spectator.tick()', fakeAsync(() => {
+    twainService = jest.spyOn(twainServiceSpy, 'getQuote').mockReturnValue(throwError(new Error('TwainService test failure')));
+    // twainService = jest.spyOn(twainServiceSpy, 'getQuote').mockImplementation(() => (throwError(new Error('TwainService test failure')))); // same
+    // twainService = twainServiceSpy.getQuote.mockImplementation(() => (throwError(new Error('TwainService test failure')))); // same
 
+    spectator.detectChanges();
+    expect(twainService).toHaveBeenCalled();
+
+    spectator.tick(); // tick is needed here to flush the setTimeout inside the catchError operator block
+
+    expect(errorMessage()).toMatch(/test failure/);
+    expect(errorMessage()).toBe('TwainService test failure'); // same
+
+    expect(spectator.query(byText('...'))).toBeTruthy();
+  }));
 
 });
