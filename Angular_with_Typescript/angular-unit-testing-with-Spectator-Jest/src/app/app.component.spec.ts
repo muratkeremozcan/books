b97447e7bfb8b4644d0e51a7e331a5fd68988b9f
Murat Ozcan <muratkerem@gmail.com>
2021-03-26 07:17:12 -0500

    app component spec

--- Angular_with_Typescript/angular-unit-testing-with-Spectator-Jest/src/app/app.component.spec.ts
+++ Angular_with_Typescript/angular-unit-testing-with-Spectator-Jest/src/app/app.component.spec.ts
@@ -4,9 +4,12 @@ import { WelcomeComponent } from './welcome/welcome.component';
 import { MockComponent } from 'ng-mocks';
 import { BannerComponent } from './banner/banner.component';
 import { RouterTestingModule } from '@angular/router/testing';
-// [7] testing components that include other components
-// setup the component
+import { Router, RouterLink } from '@angular/router';
+
+// [7] testing components that include routerLinks
+// setup the component with createRoutingFactory to auto-mock Router and ActivatedRoute
 // KEY: mock the internal components, use the ng-mocks library MockComponent (7.1.5). just like (6.1.5)
+// KEY: for <router-outlet> , import RouterTestingModule
 
 describe('App component', () => {
   let component: AppComponent;
@@ -29,7 +32,24 @@ describe('App component', () => {
     expect(component).toBeTruthy();
   });
 
+  it('read routerLinks with  { read: RouterLink } option', () => {
+    const link1 = spectator.query('.qa-link-1', { read: RouterLink });
+    const link2 = spectator.query('.qa-link-2', { read: RouterLink });
+    const link3 = spectator.query('.qa-link-3', { read: RouterLink });
 
+    expect(link1.routerLink).toEqual('/dashboard');
+    expect(link2.routerLink).toEqual('/heroes');
+    expect(link3.routerLink).toEqual('/about');
+  });
 
+  // it('click navigate to routes', () => {
+    // spectator.detectChanges();
+
+    // TODO: cannot click ....
+    // spectator.click('.qa-link-1');
+    // spectator.click(byText('Dashboard'));
+
+    // expect(spectator.inject(Router).navigate).toHaveBeenCalled();
+  // });
 });
 
