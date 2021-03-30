f8ec74da1d819b7a698ea087296dcc9ffcc228cf
Murat Ozcan <muratkerem@gmail.com>
2021-03-30 15:46:59 -0500

    joe eames base working

--- Angular_with_Typescript/angular-unit-testing-with-karma-joe-example/src/app/app.component.ts
+++ Angular_with_Typescript/angular-unit-testing-with-karma-joe-example/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-unit-testing-with-karma-joe-example';
+  title = 'Tour of Heroes';
 }
