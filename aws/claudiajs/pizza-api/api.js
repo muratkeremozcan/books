4ee1631a6b0bb84f4ba4e8ad19a5dd4ad847cc81
Murat Ozcan <muratkerem@gmail.com>
2021-05-19 07:44:05 -0500

    claudia ch3

--- aws/claudiajs/pizza-api/api.js
+++ aws/claudiajs/pizza-api/api.js
@@ -1,9 +1,65 @@
 const Api = require('claudia-api-builder')
 const api = new Api()
 
-api.get('/pizzas', () =>  ['Capricciosa', 'Quattro Formaggi', 'Napoletana', 'Margherita']);
+const getPizzas = require('./handlers/get-pizzas')
+const createOrder = require('./handlers/create-order')
+const updateOrder = require('./handlers/update-order')
+const deleteOrder = require('./handlers/delete-order')
+
+api.get('/', () => 'Welcome to Pizza API');
+
+api.get('/pizzas', () => getPizzas());
+
+api.get('/pizzas/{id}', request => getPizzas(request.pathParams.id), {
+  error: 404
+});
+
+api.post('/orders', request => createOrder(request.body), {
+  success: 201,
+  error: 400
+});
+
+api.put('/orders/{id}', request => updateOrder(request.pathParams.id, request.body), {
+  error: 400
+});
+
+api.delete('/orders/{id}', request => deleteOrder(request.pathParams.id), {
+  error: 400
+});
 
 module.exports = api;
 
 // to deploy: claudia create --region us-east-1 --api-module api
+// to update: claudia update --cache-api-config apiConfig
+// to test: https://2afo7guwib.execute-api.us-east-1.amazonaws.com/latest/pizzas
 
+/*
+created a user with very high policies:
+IAMFullAccess
+AmazonS3FullAccess
+AmazonAPIGatewayPushToCloudWatchLogs
+AmazonDynamoDBFullAccess
+AmazonAPIGatewayAdministrator
+AWSCodeDeployRoleForCloudFormation
+AWSCloudFormationFullAccess
+AWSLambda_FullAccess
+claudia (custom policy)
+
+the custom policy is saved in the root as customPolicyForAws.json
+
+after the deploy the response is
+
+{
+  "lambda": {
+    "role": "pizza-api-executor",
+    "name": "pizza-api",
+    "region": "us-east-1"
+  },
+  "api": {
+    "id": "2afo7guwib",
+    "module": "api",
+    "url": "https://2afo7guwib.execute-api.us-east-1.amazonaws.com/latest"
+  }
+}
+
+*/
