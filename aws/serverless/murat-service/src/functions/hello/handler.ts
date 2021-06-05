a7401de3db5705f5d00cb3ebd6f38bac6d836e50
Murat Ozcan <muratkerem@gmail.com>
2021-06-05 06:58:22 -0500

    working serverless framework example

--- aws/serverless/murat-service/src/functions/hello/handler.ts
+++ aws/serverless/murat-service/src/functions/hello/handler.ts
@@ -12,8 +12,7 @@ import schema from './schema';
 const hello: ValidatedEventAPIGatewayProxyEvent<typeof schema> = async (event) => {
   console.log('hello works');
   return formatJSONResponse({
-    message: `Hello ${event.body.name}, welcome to the exciting Serverless world!`,
-    event,
+    message: `Hello ${event.body.name}, welcome to the exciting Serverless world!`
   });
 }
 
