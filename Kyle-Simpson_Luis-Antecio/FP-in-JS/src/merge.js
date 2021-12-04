5fc2f718271f2cc30a43f5ec2cdaf04a5c677f21
Murat Ozcan <muratkerem@gmail.com>
2021-12-04 14:28:15 -0600

    dreaded ds store

---
+++ Kyle-Simpson_Luis-Antecio/FP-in-JS/src/merge.js
@@ -0,0 +1,23 @@
+import R from "ramda";
+import _ from "lodash";
+
+const obj1 = {
+  characters: [{ name: "barney" }, { name: "fred" }],
+  a: 10,
+  p: { x: 10, y: 20 },
+  k: "k",
+};
+
+const obj2 = {
+  characters: [{ age: 36 }, { age: 40 }],
+  a: 20,
+  p: { x: 20, z: 30 },
+  t: "t",
+};
+
+// lodash merge example
+// (obj1 <- obj2), shallow merge, source takes precedence on duplicates
+_.merge(obj1, obj2) 
+
+// (ob1j -> obj2), obj2 takes precedence and takes in the extra keys from obj1
+R.merge(obj1, obj2) 
