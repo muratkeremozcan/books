aaee5eefd3289fa7eb8b4b09c1380c663e53ccf7
Murat Ozcan <muratkerem@gmail.com>
2022-12-06 08:03:03 -0600

    skeptic's guide ch1

--- 99_MuratsNinjaExercises/fp-skeptics-guide/01.1-map-pipe.js
+++ 99_MuratsNinjaExercises/fp-skeptics-guide/01.1-map-pipe.js
@@ -63,14 +63,6 @@ const dataForTemplateR = R.pipe(
 )(notificationData)
 R.equals(dataForTemplateR, dataForTemplate) //?
 
-////// use functors
-// terminology:
-// Container : a type that wraps a value
-// Functor : Container + has map
-// Pointed Functor : Functor + has of
-// Monad : Pointed Functor + has flatten
-// all Boxed types are Monads
-
 const map = f => functor => functor.map(f)
 /** The pipe function uses the spread operator to turn all but the first argument into an array.
  * Then it passes that first argument to the first function. And the result of that to the next function. And so on. */
--- 99_MuratsNinjaExercises/fp-skeptics-guide/01.2-error-handling.js
+++ 99_MuratsNinjaExercises/fp-skeptics-guide/01.2-error-handling.js
@@ -1,3 +1,6 @@
+import R from 'ramda'
+import notificationData from './notificationData.json'
+
 /** we catch the error, and return undefined if the response doesn’t parse.
  * Remember safe-json-parse.ts
  */
@@ -10,7 +13,8 @@ const parseJSON = dataFromServer => {
   }
 }
 
-// problem: each of the fns needs to get littered with error handling
+// PROBLEM 1: each of the fns needs to get littered with error handling
+// PROBLEM 2: we have to pass the notificationData as arg to all functions but buildLinkToSource (it had it before already)
 
 const getSet = (getKey, setKey, transform) => obj => ({
   ...obj,
@@ -68,3 +72,43 @@ const addIcon = notification => {
     return undefined
   }
 }
+
+const withDates = notificationData.map(addReadableDate(notificationData))
+const sanitized = withDates.map(sanitizeMessage(notificationData))
+const withSenders = sanitized.map(buildLinkToSender(notificationData))
+const withSources = withSenders.map(buildLinkToSource)
+const dataForTemplate = withSources.map(addIcon(notificationData))
+
+// use chain (intermediate)
+const dataForTemplateC = notificationData
+  .map(addReadableDate(notificationData))
+  .map(sanitizeMessage(notificationData))
+  .map(buildLinkToSender(notificationData))
+  .map(buildLinkToSource)
+  .map(addIcon(notificationData))
+R.equals(dataForTemplateC, dataForTemplate) //?
+
+// use ramda to compose the functions
+const dataForTemplateR = R.pipe(
+  R.map(addReadableDate(notificationData)),
+  R.map(sanitizeMessage(notificationData)),
+  R.map(buildLinkToSender(notificationData)),
+  R.map(buildLinkToSource),
+  R.map(addIcon(notificationData)),
+)(notificationData)
+R.equals(dataForTemplateR, dataForTemplate) //?
+
+const map = f => functor => functor.map(f)
+/** The pipe function uses the spread operator to turn all but the first argument into an array.
+ * Then it passes that first argument to the first function. And the result of that to the next function. And so on. */
+const pipe = (x0, ...fns) => fns.reduce((x, f) => f(x), x0)
+
+const dataFoTemplateF = pipe(
+  notificationData,
+  map(addReadableDate(notificationData)),
+  map(sanitizeMessage(notificationData)),
+  map(buildLinkToSender(notificationData)),
+  map(buildLinkToSource),
+  map(addIcon(notificationData)),
+)
+R.equals(dataFoTemplateF, dataForTemplate) //?
